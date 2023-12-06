cd89a0ca2812aae502781f498b22daef21392b22
alexjava92 <autoexpert37@yandex.ru>
2023-12-06 13:49:07 +0300

    init

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ bot.on('message', async (msg) => {
 
 app.post('/web-data', async (req, res) => {
     const {queryId, products = [], totalPrice} = req.body;
+    console.log(req.body)
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
